c506a9212232943ba069f3a2c606da8db0ef882f
adambens <adambens@umich.edu>
2018-03-18 21:16:48 -0400

    fixed

--- examplelec3.py
+++ examplelec3.py
@@ -4,13 +4,24 @@ app = Flask(__name__)
 
 @app.route('/oranges')
 def lemons():
-    title = "My Ice Cream Form"
+
+    title_var = "My Ice Cream Form"
+    options = ["Chocolate", "Vanilla", "Superman", "Pistachio", "Anything I Want!"]
     # Add code -- what type should options hold?
     return render_template('seeform.html',title=title_var, lst_stuff=options)
 
 @app.route('/apples')
 def plants():
     ## Add code here
+    ## FIX THIS
+    
+
+    flavor_options = request.args #.get("flavor")
+    #flavor_options = result.items()
+    
+    name = request.args.get("name", "None")
+    name_len = len(name)
+
     return render_template('results.html',flavors=flavor_options, name_len=name_len, name=name)
 
 
